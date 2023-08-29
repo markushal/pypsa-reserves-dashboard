d9c212052d20cf789e789581d3b6f8b79317e118
Markus Haller <m.haller@oeko.de>
2023-08-29 13:40:08 +0200

    poetry: add glpk as optional package

--- dashboard.py
+++ dashboard.py
@@ -19,7 +19,7 @@ n = db.create_network(settings)
 db.add_balancing_constraints(n, settings["contingency"])
 
 # solve model:
-n.optimize.solve_model(solver="gurobi", assign_all_duals=True)
+n.optimize.solve_model(assign_all_duals=True)
 
 res = db.concat_results(n)
 

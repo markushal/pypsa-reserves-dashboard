1d293f7eccbdb5cc9c02ae13d49d44404f8484c4
Markus Haller <m.haller@oeko.de>
2023-11-20 12:07:36 +0100

    improve about text

--- content/tab_about.py
+++ content/tab_about.py
@@ -4,6 +4,7 @@ import streamlit as st
 def create_tab_about():
     st.markdown(
         """
+### About this app:
 This dashboards lets you define the parameters of a small energy system model, 
 builds the model in pyPSA, solves it, and lets you inspect the results. 
 It was build to demonstrate implementing reserve power requirements in pyPSA, 
@@ -17,7 +18,14 @@ is outlined in tab "methodology".
 If you change any setting, the changes are applied to the model definition, the new model
 is solved, and the results are displayed immediately. The model is solved using the HiGHs solver. 
 
+### Work in progress!
+
+This is a draft version. There will be bugs and inconsistent behaviour. 
+
+### Source code on GitHub: 
+
+The source code of this app is available on GitHub:  https://github.com/markushal/pypsa-reserves-dashboard
+
 
 """
     )
-    pass

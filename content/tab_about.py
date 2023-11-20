0d29b0dcf7614c84d32ec528ae4dba9bfa542bcf
Markus Haller <m.haller@oeko.de>
2023-11-20 11:57:12 +0100

    format about tab

--- content/tab_about.py
+++ content/tab_about.py
@@ -6,21 +6,16 @@ def create_tab_about():
         """
 This dashboards lets you define the parameters of a small energy system model, 
 builds the model in pyPSA, solves it, and lets you inspect the results. 
-
 It was build to demonstrate implementing reserve power requirements in pyPSA, 
 but you can use it to learn about the behaviour of a "vanilla" pyPSA model as well. 
 
-All settings are made in the sidebar to the left of the screen. 
-
-Parameters and results are displayed in the "results" tab. 
-
-The methodology used to represent the satisfaction of reserve power requirements 
+- All settings are made in the sidebar to the left of the screen. 
+- Parameters and results are displayed in the "results" tab. 
+- The methodology used to represent the satisfaction of reserve power requirements 
 is outlined in tab "methodology". 
 
 If you change any setting, the changes are applied to the model definition, the new model
-is solved, and the results are displayed immediately. 
-
-The model is solved using the HiGHs solver. 
+is solved, and the results are displayed immediately. The model is solved using the HiGHs solver. 
 
 
 """

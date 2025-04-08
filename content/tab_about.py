42440d7db137a60800966965c5b72127a073eba8
j.aschauer <j.aschauer@oeko.de>
2025-04-08 10:13:21 +0200

    add funding statement in about tab

--- content/tab_about.py
+++ content/tab_about.py
@@ -7,6 +7,9 @@ import streamlit as st
 
 def create_tab_about():
     """Render content of about page."""
+    st.markdown("This dashboard was developed as part of the IND-E project.")
+    st.image(Path(__file__).parent.parent / "static" / "BMWK_Fz_2017_Web2x_en.gif")
+
     st.markdown(
         (Path(__file__).parent.parent / "static" / "about.md").read_text(
             encoding="utf-8"
